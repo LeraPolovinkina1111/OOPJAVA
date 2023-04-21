a16320f4cd9c5893d640daca89db0d13b239f258
Valera <leo.plv@bk.ru>
2023-04-21 21:43:31 +0500

    dz2

---
+++ OOPdz/dz2/Task.java
@@ -0,0 +1,9 @@
+package OOPdz.dz2;
+
+public class Task {
+    public static void main(String[] args) {
+
+        Processing process = new Processing();
+        process.Start();
+    }
+}
---
+++ OOPdz/dz2/sourse.java
@@ -0,0 +1,23 @@
+package OOPdz.dz2;
+
+public class sourse {
+    private double num;
+    private String operator;
+
+    public double getNum() {
+        return num;
+    }
+
+    public void setNum(double num) {
+        this.num = num;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
+
